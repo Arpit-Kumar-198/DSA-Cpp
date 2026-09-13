1915b468a549f2074b8c8c1814fbc2a33d38e608
Arpit Kumar <arpitkumar19842@gmail.com>
2026-09-13 12:47:30 +0530

    revise: refresh array and vector fundamentals

--- Practise/practise.cpp
+++ Practise/practise.cpp
@@ -1,12 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
+void factorial(int n, vector<int>& fact){
+    int carry = 0;
+    if(n == 0 || n == 1)
+        return;
 
-int solve(int n){
-    
+    for (int i = 2; i <= n; i++){
+        for (int j = 0; j < fact.size(); j++){
+            int x = fact[j] * i + carry;
+            carry = x / 10;
+            fact[j] = x % 10;
+        }
+        while(carry > 0){
+            fact.push_back(carry % 10);
+            carry /= 10;
+        }
+    }
+    return;
 }
 int main(){
+    cout << "Enter a number: ";
     int n;
     cin >> n;
-    cout << solve(n);
-    return 0;
-}
+    vector<int> fact;
+    fact.push_back(1);
+    factorial(n, fact);
+    reverse(fact.begin(), fact.end());
+    cout << "Factorial of " << n << ": ";
+    for (int i = 0; i < fact.size(); i++)
+        cout << fact[i];
+
+        return 0;
+} 
